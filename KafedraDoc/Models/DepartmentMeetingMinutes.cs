f9af3f5ec1291a7a2bdf684a4446c4e1d04d522e
zakir <zakiralekperov1998@gmail.com>
2024-09-21 09:01:25 +0300

    Добавлена модель протоколо заседания кафедры

---
+++ KafedraDoc/Models/DepartmentMeetingMinutes.cs
@@ -0,0 +1,33 @@
+﻿namespace KafedraDoc.Models
+{
+    /// <summary>
+    /// Представляет протокол заседания кафедры.
+    /// </summary>
+    public class DepartmentMeetingMinutes
+    {
+        /// <summary>
+        /// Уникальный идентификатор протокола.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Дата заседания кафедры.
+        /// </summary>
+        public DateTime MeetingDate { get; set; }
+
+        /// <summary>
+        /// Повестка дня заседания.
+        /// </summary>
+        public string Agenda { get; set; }
+
+        /// <summary>
+        /// Принятые решения на заседании.
+        /// </summary>
+        public string Decisions {  get; set; }
+
+        /// <summary>
+        /// Участники заседания.
+        /// </summary>
+        public string Participants { get; set; }
+    }
+}
